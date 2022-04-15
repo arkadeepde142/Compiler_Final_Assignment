13c62408348bc8d6d3edadef7d6b3eeaf58fa19a
Arkadeep De <arkadeepde142@gmail.com>
2022-04-15 22:00:21 +0530

    Parsing Table Generated

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,105 @@
 #include <iostream>
 #include <clexer.hpp>
+#include <iomanip>
 #include <parser/parser.hpp>
 using namespace std;
 
 int main()
 {
-    CLexer lex;
+    // CLexer lex;
 
-    string s = "int func(int x1, int x2); \n int func(int x1, int x2){return x1 + x2;} \n void main(){ int z = func(2, 3);}";
+    // string s = "int func(int x1, int x2); \n int func(int x1, int x2){return x1 + x2;} \n void main(){ int z = func(2, 3);}";
 
-    unsigned long start = 0;
-    while (start < s.size())
+    // unsigned long start = 0;
+    // while (start < s.size())
+    // {
+    //     start = lex.lex(s, start);
+    //     if (start > s.size())
+    //     {
+    //         cout << "Error at line " << lex.getLine() << endl;
+    //     }
+    // }
+
+    parser::Grammar<string> grammar{
+        {"E", {
+                  {{"T", "E'"}, [](vector<string *>) {}},
+              }},
+        {"E'", {{{"+", "T", "E'"}, [](vector<string *>) {}}, {{"#"}, [](vector<string *>) {}}}},
+        {"T", {
+                  {{"F", "T'"}, [](vector<string *>) {}},
+              }},
+        {"T'", {{{"*", "F", "T'"}, [](vector<string *>) {}}, {{"#"}, [](vector<string *>) {}}}},
+        {"F", {
+                  {{"(", "E", ")"}, [](vector<string *>) {}},
+                  {{"id"}, [](vector<string *>) {}},
+              }},
+    };
+
+    parser::Parser parser(grammar, "#"s, "E"s);
+
+    // grammar["A"] = {
+    //         {
+    //             {"a"}, [](vector<string*>){}
+
+    //         },
+    //         {
+    //             {"#"}, [](vector<string*>){}
+    //         }
+    //     };
+
+    // grammar["B"] = {
+    //         {
+    //             {"A"}, [](vector<string *>){}
+    //         }
+    // };
+    // auto firstSets = parser::generateFirstSet(grammar, "#"s);
+    // auto followSets = parser::generateFollowSet(grammar, "#"s, firstSets, "E"s);
+
+    // cout << "First Sets:\n";
+    // for (auto const &[symbol, set] : firstSets)
+    // {
+    //     cout << symbol << " -> { ";
+    //     for (auto a : set)
+    //     {
+    //         cout << a << ", ";
+    //     }
+    //     cout << "\b\b }" << endl;
+    // }
+
+    // cout << "Follow Sets:\n";
+    // for (auto const &[symbol, set] : followSets)
+    // {
+    //     cout << symbol << " -> { ";
+    //     for (auto a : set)
+    //     {
+    //         cout << (a.empty() ? "$" : a) << ", ";
+    //     }
+    //     cout << "\b\b }" << endl;
+    // }
+
+    // auto a = parser::firstSetOf({"F", "T'"}, firstSets,"#"s);
+    // for(auto t: a)
+    // {
+    //     cout << t << " ";
+    // }
+    // cout << endl;
+    auto table = parser::createParseTable(grammar, "#"s, "E"s);
+    for(auto const& [nonTerminal, m]: table)
     {
-        start = lex.lex(s, start);
-        if (start > s.size())
+        for(auto const& [terminal, production]: m)
         {
-            cout << "Error at line " << lex.getLine() << endl;
+            cout << "(";
+            cout << quoted(nonTerminal);
+            cout << ", ";
+            cout << quoted(terminal.empty() ? "$": terminal);
+            cout << ")";
+            cout << " ";
+            cout << production.first << " -> ";
+            for(auto symbol: production.second.first)
+            {
+                cout << symbol;
+            }
+            cout << endl;
         }
     }
 }
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -10,40 +10,55 @@ namespace parser
     using namespace std;
 
     template <typename Symbol>
-    using Grammar = unordered_map<Symbol, vector<pair<vector<Symbol>, function<void(vector<Symbol &>)>>>>;
+    using Grammar = unordered_map<Symbol, vector<pair<vector<Symbol>, function<void(vector<Symbol *>)>>>>;
 
     template <typename Symbol>
-    using Production = pair<Symbol, vector<Symbol>>;
+    using Production = pair<Symbol, pair<vector<Symbol>, function<void(vector<Symbol *>)>>>;
 
     template <typename Symbol>
-    unordered_map<Symbol, unordered_set<Symbol>> generateFirstSet(Grammar<Symbol> const& grammar, Symbol const& epsilon);
+    using ParseTable = unordered_map<Symbol, unordered_map<Symbol, Production<Symbol>>>;
 
     template <typename Symbol>
-    void generateFirstSetRecur(Grammar<Symbol> const& grammar, Symbol symbol,unordered_map<Symbol, unordered_set<Symbol>>& sets, Symbol const& epsilon);
+    unordered_map<Symbol, unordered_set<Symbol>> generateFirstSet(Grammar<Symbol> const &grammar, Symbol const &epsilon);
 
     template <typename Symbol>
-    unordered_map<Symbol, unordered_set<Symbol>> generateFollowSet(Grammar<Symbol> const& grammar, Symbol const& epsilon);
+    void generateFirstSetRecur(Grammar<Symbol> const &grammar, Symbol symbol, unordered_map<Symbol, unordered_set<Symbol>> &sets, Symbol const &epsilon);
 
     template <typename Symbol>
-    void generateFollowSetRecur(Grammar<Symbol> const& grammar, Symbol symbol,unordered_map<Symbol, unordered_set<Symbol>>& sets, Symbol const& epsilon);
+    unordered_map<Symbol, unordered_set<Symbol>> generateFollowSet(Grammar<Symbol> const &grammar, Symbol const &epsilon, unordered_map<Symbol, unordered_set<Symbol>> const &firstSets, Symbol start);
+
+    template <typename Symbol>
+    void generateFollowSetRecur(Grammar<Symbol> const &grammar,
+                                Symbol symbol,
+                                unordered_map<Symbol, unordered_set<Symbol>> &sets,
+                                Symbol epsilon,
+                                unordered_map<Symbol, unordered_set<Symbol>> const &firstSets,
+                                Symbol start);
+    
+    template <typename Symbol>
+    unordered_set<Symbol> firstSetOf(vector<Symbol> const& production, unordered_map<Symbol, unordered_set<Symbol>> const &firstSets, Symbol epsilon);
+
+    template <typename Symbol>
+    ParseTable<Symbol> createParseTable(Grammar<Symbol> const& grammar, Symbol epsilon, Symbol startSymbol);
 
     template <typename Symbol>
     class Parser
     {
 
     private:
-        const Grammar<Symbol> grammar;
+        const ParseTable<Symbol> parseTable;
+        const Symbol startSymbol;
         const Symbol epsilon;
 
     public:
-        Parser(Grammar<Symbol> grammar, Symbol epsilon);
+        Parser(Grammar<Symbol> grammar, Symbol epsilon, Symbol start);
         vector<Production<Symbol>> getLMD(vector<Symbol> const &symbols) const;
-
     };
 
     template <typename Symbol>
-    Parser<Symbol>::Parser(Grammar<Symbol> grammar, Symbol epsilon) : grammar(grammar), epsilon(epsilon)
+    Parser<Symbol>::Parser(Grammar<Symbol> grammar, Symbol epsilon, Symbol start) : parseTable(createParseTable(grammar, epsilon, start)), epsilon(epsilon), startSymbol(start)
     {
+        
     }
 
     template <typename Symbol>
@@ -52,51 +67,207 @@ namespace parser
     }
 
     template <typename Symbol>
-    void generateFirstSetRecur(Grammar<Symbol> const& grammar, Symbol symbol, unordered_map<Symbol, unordered_set<Symbol>>& sets, Symbol const& epsilon)
+    unordered_map<Symbol, unordered_set<Symbol>> generateFirstSet(Grammar<Symbol> const &grammar, Symbol const &epsilon)
     {
-        for(auto const& production: grammar.at(symbol))
+        unordered_map<Symbol, unordered_set<Symbol>> sets;
+        for (auto [s, _] : grammar)
         {
-            unordered_set<Symbol> tempSet;
-            for(auto const& _symbol: production)
-            {
-                if(grammar.find(_symbol) == grammar.end() || _symbol == epsilon)
-                {
-                    tempSet.insert(_symbol);
-                    break;
-                }
-                else
-                {
-                    generateFirstSetRecur(grammar, _symbol, sets);
-                    set_union(
-                        begin(tempSet),
-                        end(tempSet),
-                        begin(sets[_symbol]),
-                        end(sets[_symbol]),
-                        inserter(tempSet, end(tempSet))
-                    );
+            generateFirstSetRecur(grammar, s, sets, epsilon);
+        }
 
-                    if(sets[_symbol].find(epsilon) == sets[_symbol].end())
+        return sets;
+    }
+
+    template <typename Symbol>
+    void generateFirstSetRecur(Grammar<Symbol> const &grammar, Symbol symbol, unordered_map<Symbol, unordered_set<Symbol>> &sets, Symbol const &epsilon)
+    {
+        if (sets.find(symbol) == sets.end())
+        {
+            for (auto const &[production, _] : grammar.at(symbol))
+            {
+                unordered_set<Symbol> tempSet;
+                for (auto const &_symbol : production)
+                {
+                    if (grammar.find(_symbol) == grammar.end() || _symbol == epsilon)
                     {
-                        tempSet.erase(epsilon);
+                        if (_symbol != epsilon)
+                            tempSet.erase(epsilon);
+                        tempSet.insert(_symbol);
                         break;
                     }
-                }
-            }
+                    else
+                    {
+                        generateFirstSetRecur(grammar, _symbol, sets, epsilon);
+                        set_union(
+                            begin(tempSet),
+                            end(tempSet),
+                            begin(sets[_symbol]),
+                            end(sets[_symbol]),
+                            inserter(tempSet, end(tempSet)));
 
-            set_union(
-                        begin(tempSet),
-                        end(tempSet),
-                        begin(sets[symbol]),
-                        end(sets[symbol]),
-                        inserter(sets[symbol], end(sets[symbol]))
-                    );
+                        if (sets[_symbol].find(epsilon) == sets[_symbol].end())
+                        {
+                            tempSet.erase(epsilon);
+                            break;
+                        }
+                    }
+                }
+
+                set_union(
+                    begin(tempSet),
+                    end(tempSet),
+                    begin(sets[symbol]),
+                    end(sets[symbol]),
+                    inserter(sets[symbol], end(sets[symbol])));
+            }
         }
     }
 
     template <typename Symbol>
-    void generateFollowSetRecur(Grammar<Symbol> const& grammar, Symbol symbol,unordered_map<Symbol, unordered_set<Symbol>>& sets)
+    void generateFollowSetRecur(Grammar<Symbol> const &grammar,
+                                Symbol symbol,
+                                unordered_map<Symbol, unordered_set<Symbol>> &sets,
+                                Symbol epsilon,
+                                unordered_map<Symbol, unordered_set<Symbol>> const &firstSets,
+                                Symbol start)
     {
+        if (sets.find(symbol) == sets.end() || symbol == start)
+        {
+            for (auto const &[_symbol, productions] : grammar)
+            {
+                for (auto const &[production, _] : productions)
+                {
+                    unordered_set<Symbol> tempSet;
+                    auto match = find(begin(production), end(production), symbol);
+
+                    if (match != production.end())
+                    {
+                        while (true)
+                        {
+                            ++match;
+                            if (match == production.end())
+                            {
+                                if (symbol != _symbol)
+                                {
+                                    generateFollowSetRecur(grammar, _symbol, sets, epsilon, firstSets, start);
+                                    set_union(
+                                        begin(tempSet),
+                                        end(tempSet),
+                                        begin(sets.at(_symbol)),
+                                        end(sets.at(_symbol)),
+                                        inserter(tempSet, end(tempSet)));
+                                }
+                                break;
+                            }
+                            else
+                            {
+                                if (grammar.find(*match) == grammar.end())
+                                {
+                                    tempSet.insert(*match);
+                                    break;
+                                }
+                                else
+                                {
+                                    set_union(
+                                        begin(tempSet),
+                                        end(tempSet),
+                                        begin(firstSets.at(*match)),
+                                        end(firstSets.at(*match)),
+                                        inserter(tempSet, end(tempSet)));
+
+                                    tempSet.erase(epsilon);
+
+                                    if (firstSets.at(*match).find(epsilon) == firstSets.at(*match).end())
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    set_union(
+                        begin(tempSet),
+                        end(tempSet),
+                        begin(sets[symbol]),
+                        end(sets[symbol]),
+                        inserter(sets[symbol], end(sets[symbol])));
+                }
+            }
+        }
+    }
+
+    template <typename Symbol>
+    unordered_map<Symbol, unordered_set<Symbol>> generateFollowSet(Grammar<Symbol> const &grammar,
+                                                                   Symbol const &epsilon, unordered_map<Symbol, unordered_set<Symbol>> const &firstSets,
+                                                                   Symbol start)
+    {
+        unordered_map<Symbol, unordered_set<Symbol>> sets;
+        sets[start].insert(Symbol());
+        for (auto [s, _] : grammar)
+        {
+            generateFollowSetRecur(grammar, s, sets, epsilon, firstSets, start);
+        }
+        return sets;
+    }
+
+    template <typename Symbol>
+    unordered_set<Symbol> firstSetOf(vector<Symbol> const& production, unordered_map<Symbol, unordered_set<Symbol>> const &firstSets, Symbol epsilon)
+    {
+        unordered_set<Symbol> result;
+        for(Symbol symbol: production)
+        {
+            if(firstSets.find(symbol) == firstSets.end())
+            {
+                result.erase(epsilon);
+                result.insert(symbol);
+                break;
+            }
+
+            set_union(
+            begin(firstSets.at(symbol)),
+            end(firstSets.at(symbol)),
+            begin(result),
+            end(result),
+            inserter(result, end(result)));
+
+            if(firstSets.at(symbol).find(epsilon) == firstSets.at(symbol).end())
+            {
+                result.erase(epsilon);
+                break;
+            }
+        }
+        return result;
+    }
+
+    template <typename Symbol>
+    ParseTable<Symbol> createParseTable(Grammar<Symbol> const& grammar, Symbol epsilon, Symbol startSymbol)
+    {
+        ParseTable<Symbol> parseTable;
         
+        auto firstSets = generateFirstSet(grammar, epsilon);
+        auto followSets = generateFollowSet(grammar, epsilon, firstSets, startSymbol);
+
+        for(auto const& [symbol, productions]: grammar)
+        {
+            for(auto const& [production, action]: productions)
+            {
+                auto firstSymbols = firstSetOf(production, firstSets, epsilon);
+                if(firstSymbols.find(epsilon) != firstSymbols.end())
+                {
+                    for(auto const& followSymbol: followSets.at(symbol))
+                    {
+                        parseTable[symbol][followSymbol] = {symbol, {production, action}};
+                    }
+                    firstSymbols.erase(epsilon);
+                }
+                for(auto const& firstSymbol: firstSymbols)
+                {
+                    parseTable[symbol][firstSymbol] = {symbol, {production, action}};
+                }
+            }
+        }
+        return parseTable;
     }
 
 } // namespace parser
